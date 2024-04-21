16fe80c234fa4b49b07fcc4d657b8360becae51d
Najmi Irfan <irfanizhak@gmail.com>
2024-04-21 12:50:31 +0800

    add redux config

---
+++ src/lib/reducers/test.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    loading: true
+}
+
+export const testSlice = createSlice({
+    name: 'test',
+    initialState,
+    reducers: {
+
+    }
+})
